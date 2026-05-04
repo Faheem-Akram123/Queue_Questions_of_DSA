12010bd334e1c3535ad1c04784a7a4f4a69a6e7e
Faheem-Akram123 <fa1712119@gmail.com>
2026-05-04 19:46:52 +0500

    add question 2

--- question2.cpp
+++ question2.cpp
@@ -3,22 +3,4 @@
 #include "../DSA-headerFiles-using-Cpp/myQueue.h"
 #include <string>
 
-int main()
-{
-    string str;
-    cout << "Enter string : ";
-    cin >> str;
-
-    myQueue<char> q1(str.length());
-    myQueue<char> q2(str.length());
-
-    for (int i = 0; i < str.length(); i++)
-    {
-        q1.enqueue(str[i]);
-    }
-    q1.display();
-
-    q2.enqueue(q1.dequeue());
-
-    
-}
+ 
